b34c31cd9ea6f46af1275b20b78bc53148da33c3
Nedelcu Cosmin <cosminnedelcu0903@gmail.com>
2025-07-31 22:58:34 +0300

    Health Bar UI with damage and healing ->Successfully

--- joc_cu_edi/Pirma_incercare/Assets/Scripts/PlayerHealth.cs
+++ joc_cu_edi/Pirma_incercare/Assets/Scripts/PlayerHealth.cs
@@ -1,14 +1,25 @@
 using UnityEngine;
+using TMPro;
 
 public class PlayerHealth : MonoBehaviour
 {
 
     public int currentHealth;
     public int maxHealth = 3;
+    public TMP_Text healthText;
+    public Animator healthTextAnim;
+
+    private void Start()
+    {
+        healthText.text = "HP: " + currentHealth + " / " + maxHealth;
+    }
 
     public void ChangeHealth(int amount)
     {
         currentHealth += amount;
+        healthTextAnim.Play("Textupdate");
+
+        healthText.text = "HP: " + currentHealth + " / " + maxHealth;
 
         if (currentHealth <= 0)
         {
