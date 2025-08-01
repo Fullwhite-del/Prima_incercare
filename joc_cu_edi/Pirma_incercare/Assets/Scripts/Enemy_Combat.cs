552581988ca4015f63569466025daac797770255
Nedelcu Cosmin <cosminnedelcu0903@gmail.com>
2025-08-01 22:13:43 +0300

    Enemy animation and tracking control

--- joc_cu_edi/Pirma_incercare/Assets/Scripts/Enemy_Combat.cs
+++ joc_cu_edi/Pirma_incercare/Assets/Scripts/Enemy_Combat.cs
@@ -5,6 +5,10 @@ public class Enemy_Combat : MonoBehaviour
     public int damage = 1;
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        collision.gameObject.GetComponent<PlayerHealth>().ChangeHealth(-damage);
+        if(collision.gameObject.tag == "Warrior_Blue")
+        {
+            collision.gameObject.GetComponent<PlayerHealth>().ChangeHealth(-damage);
+
+        }
     }
 }
---
+++ joc_cu_edi/Pirma_incercare/Assets/Scripts/Enemy_movement.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+
+public class Enemy_movement : MonoBehaviour
+{
+    public float speed;
+    private bool isChasing;
+    private int facingDirection = 1;
+    private EnemyState enemyState, newState;
+
+    private Rigidbody2D rb;
+    private Transform player;
+    private Animator anim;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        ChangeState(EnemyState.Idle);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyState == EnemyState.Chasing) // a fost if(isChasing == true)
+        {
+            if (player.position.x > transform.position.x && facingDirection == -1 ||
+                player.position.x < transform.position.x && facingDirection == 1)
+            {
+                Flip();
+            }
+            Vector2 direction = (player.position - transform.position).normalized;
+            rb.linearVelocity = direction * speed;
+        }
+            
+    }
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Warrior_Blue")
+        {
+            if(player == null) 
+            {
+                player = collision.transform;
+            }
+            //isChasing = true; 
+            ChangeState(EnemyState.Chasing);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Warrior_Blue")
+        {
+            rb.linearVelocity = Vector2.zero;
+            //isChasing = false;
+            ChangeState(EnemyState.Idle);
+
+        }
+    }
+
+    void ChangeState(EnemyState newstate)
+    {   
+        //exit the current animation
+        if(enemyState == EnemyState.Idle)
+            anim.SetBool("isidle", false);
+        else if(enemyState == EnemyState.Chasing)
+            anim.SetBool("ischasing", false);
+        
+        //update the current state
+        enemyState = newstate;
+
+        //update the new animation
+        if (enemyState == EnemyState.Idle)
+            anim.SetBool("isidle", true);
+        else if (enemyState == EnemyState.Chasing)
+            anim.SetBool("ischasing", true);
+    }
+}
+
+public enum EnemyState
+{
+    Idle,
+    Chasing
+}
